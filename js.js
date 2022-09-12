dd42e58c4140da2e8aaa8ab8d2e07d7f7845bca4
melisaapoggilicenciada <112989336+melisaapoggilicenciada@users.noreply.github.com>
2022-09-11 22:03:26 -0300

    Ejercicio 9 java script

---
+++ js.js
@@ -0,0 +1,8 @@
+var nombre = prompt("ingrese su nombre");
+var idioma = prompt("ingrese el idioma");
+var mail =  prompt ("ingrese su mail");
+var edad = prompt("ingrese su edad");
+var domicilio = prompt("ingrese su domicilio");
+
+console.log({ nombre, idioma, mail, edad, domicilio });
+document.write ("Mi nombre es: "+nombre+" hablo:"+idioma+" mi mail es: "+mail+" tengo: "+edad+" vivo en: "+domicilio+"");
